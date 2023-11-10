c2b570abb74f93811c148d5c67b0d7106ef41d0b
AndyTRY <andyren.yuen@mail.utoronto.ca>
2023-11-10 16:09:48 -0500

    Update Models
    Co-authored-by: Nazmus Saqeeb <namoosir@users.noreply.github.com>

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,5 +10,59 @@ namespace MediaTrackerAuthenticationService.Data
 
         public DbSet<PlatformConnection> PlatformConnections => Set<PlatformConnection>();
         public DbSet<User> Users => Set<User>();
+
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(
+                    e =>
+                        e.Entity is PlatformConnection
+                        && (e.State == EntityState.Added || e.State == EntityState.Modified)
+                );
+
+            foreach (var entityEntry in entries)
+            {
+                ((PlatformConnection)entityEntry.Entity).UpdatedAt = DateTime.Now;
+            }
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(
+                    e =>
+                        e.Entity is PlatformConnection
+                        && (e.State == EntityState.Added || e.State == EntityState.Modified)
+                );
+
+            foreach (var entityEntry in entries)
+            {
+                ((PlatformConnection)entityEntry.Entity).UpdatedAt = DateTime.Now;
+            }
+            
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(
+                    e =>
+                        e.Entity is PlatformConnection
+                        && (e.State == EntityState.Added || e.State == EntityState.Modified)
+                );
+
+            foreach (var entityEntry in entries)
+            {
+                ((PlatformConnection)entityEntry.Entity).UpdatedAt = DateTime.Now;
+            }
+            
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
